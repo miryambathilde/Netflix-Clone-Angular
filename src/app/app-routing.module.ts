cbc4d84f9c6af93f8a6e3ecc761577cab37a41b7
Miryam Bathilde <mbathilde@gmail.com>
2023-02-02 11:22:56 +0100

    home navbar and routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { SearchComponent } from './pages/search/search.component';
 import { MovieDetailsComponent } from './pages/movie-details/movie-details.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'search', component: SearchComponent },
   { path: 'movie/:id', component: MovieDetailsComponent },
