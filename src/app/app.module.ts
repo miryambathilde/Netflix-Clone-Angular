864a5197a2ccdcaf2ef165072b46a99af9897f6a
Miryam Bathilde <mbathilde@gmail.com>
2023-02-02 10:10:14 +0100

    structuring project: pages and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MovieDetailsComponent } from './pages/movie-details/movie-details.component';
+import { SearchComponent } from './pages/search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MovieDetailsComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
