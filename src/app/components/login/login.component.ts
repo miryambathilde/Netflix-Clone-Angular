746a3b0e882774fb752315745ca213a7faea2f53
Miryam Bathilde <mbathilde@gmail.com>
2023-02-03 11:13:19 +0100

    firebase auth,  login and register user & styles

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  formLogin: FormGroup;
+
+  constructor (private userService: UserService, private router: Router) {
+    this.formLogin = new FormGroup({
+      email: new FormControl(),
+      password: new FormControl()
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit () {
+    this.userService.login(this.formLogin.value)
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => console.log(error));
+  }
+
+  onClick () {
+    this.userService.loginWithGoogle()
+      .then(response => {
+        console.log(response);
+        this.router.navigate([ '/home' ]);
+      })
+      .catch(error => console.log(error));
+  }
+
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'login-firebase-auth-3225b',
+    appId: '1:286999975693:web:c259402950af16ba334244',
+    storageBucket: 'login-firebase-auth-3225b.appspot.com',
+    apiKey: 'AIzaSyBNw9cimeg0fKNN7fe9aWX166dlumfXAGc',
+    authDomain: 'login-firebase-auth-3225b.firebaseapp.com',
+    messagingSenderId: '286999975693',
+  },
   production: false
 };
 
